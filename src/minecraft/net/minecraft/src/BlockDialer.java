1c3bfd06fd707ca0b52bb72582b78e9529f5eb45
Jeffrey Becker <jeffrey.a.becker@gmail.com>
2012-07-19 17:37:55 -0400

    gate tracker pass 1

--- src/minecraft/net/minecraft/src/BlockDialer.java
+++ src/minecraft/net/minecraft/src/BlockDialer.java
@@ -6,4 +6,5 @@ public class BlockDialer extends Block
 	{
 		super(id, Material.rock);
 	}
+
 }
---
+++ src/minecraft/net/minecraft/src/Gate.java
@@ -0,0 +1,10 @@
+package net.minecraft.src;
+
+
+public class Gate {
+	private String _address;
+	private BlockDialer _dialer;
+	
+	public String getAddress(){ return _address;}
+	
+}
