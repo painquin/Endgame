6d49e69f755bbeb3fbecbcd4fc529d0612c0ff4a
Jeffrey Becker <jeffrey.a.becker@gmail.com>
2012-07-25 17:32:47 -0400

    added gate finding features to gate class

--- src/minecraft/net/minecraft/src/Gate.java
+++ src/minecraft/net/minecraft/src/Gate.java
@@ -2,6 +2,19 @@ package net.minecraft.src;
 
 
 public class Gate {
+	private static Set<Gate> _gates = new HashSet<Gate>();
+	
+	public static Gate findGate(String address){
+		for(Gate gate : _gates){
+			if(gate.getAddress().equals(address)){
+				return gate;
+			}
+		}
+		return null;
+	}
+
+
+
 	private String _address;
 	private int _x;
 	private int _y;
